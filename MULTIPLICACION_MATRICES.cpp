e2a6500453229ca1deb4986e02f0f788c647774e
Rodrigo <rodvaudio@gmail.com>
2020-03-05 21:59:43 -0600

    -Declaraciones afuera del main(), suponiendo que la matriz no cambia ni su tamaño. -Funcion para imprimir producto. -Quite algunas constantes, aun se pueden quitar filas y columnas y solo poner un tamaño, valen lo mismo.

--- MULTIPLICACION_MATRICES.cpp
+++ MULTIPLICACION_MATRICES.cpp
@@ -1,87 +1,80 @@
 #include <stdio.h>
 #include <math.h>
+#include <string.h>
+
+#define FILAS_MATRIZ 7
+#define COLUMNAS_MATRIZ 7
+
+//0 == infinito
+int matrizA[FILAS_MATRIZ][COLUMNAS_MATRIZ] = {
+  {0, 2, 2, 0, 0, 8, 6},
+  {2, 0, 6, 2, 3, 3, 0},
+  {2, 6, 0, 3, 0, 0, 4},
+  {0, 2, 3, 0, 1, 0, 2},
+  {0, 3, 0, 1, 0, 1, 0},
+  {8, 3, 0, 0, 1, 0, 5},
+  {6, 0, 4, 2, 0, 5, 0}
+
+};
+int matrizB[FILAS_MATRIZ][COLUMNAS_MATRIZ];
+
+int producto[FILAS_MATRIZ][COLUMNAS_MATRIZ];
+int potencia = 0;
 
 
-#define FILAS_MATRIZ_A 5
-#define COLUMNAS_MATRIZ_A 5
-#define FILAS_MATRIZ_DEF 5
-#define COLUMNAS_MATRIZ_DEF 5
+void imprimirMatriz(int matriz[FILAS_MATRIZ][COLUMNAS_MATRIZ] ){
+  for (int i = 0; i < FILAS_MATRIZ; i++){
+      for (int j = 0; j < COLUMNAS_MATRIZ; j++){
+          printf("%d\t", matriz[i][j]);
+      }
+      printf("\n");
+  }
+  printf("\n");
+}
 
-
-
-
-
-int potencia=3;
-
-
-int main(void) {
-
-    int matrizA[FILAS_MATRIZ_A][COLUMNAS_MATRIZ_A] = {
- 			{0, 1, 0, 1, 0},
-            {1, 0, 1, 0, 1},
-            {0, 1, 0, 1, 1},
-			{1, 0, 1, 0, 0},
-			{0, 1, 1, 0, 0},
-			
-           
-    };
-   
-   
-    int matrizDef[FILAS_MATRIZ_DEF][COLUMNAS_MATRIZ_DEF] = {
-            {0, 1, 0, 1, 0},
-            {1, 0, 1, 0, 1},
-            {0, 1, 0, 1, 1},
-			{1, 0, 1, 0, 0},
-			{0, 1, 1, 0, 0},
-			
-			
-            
-			
-    };
-   
-    
-    int producto[FILAS_MATRIZ_DEF][COLUMNAS_MATRIZ_DEF];
-   /*nt final[FILAS_MATRIZ_FINAL][COLUMNAS_MATRIZ_FINAL];*/
-    
-   
-    for (int a = 0; a < COLUMNAS_MATRIZ_DEF; a++){
-	
-    	
-    	
-        
-        for (int i = 0; i < FILAS_MATRIZ_A; i++) {
-            int suma = 0;
-           
-            for ( int j = 0; j < COLUMNAS_MATRIZ_A; j++) {
-                
-                suma += matrizA[i][j] * matrizA[j][a];
-            }
-            
-            producto[i][a] = suma;
-            
-        }
-        
-   
-    } 
-	
-
-    printf("Imprimiendo producto\n");
-    for (int i = 0; i < FILAS_MATRIZ_DEF; i++){
-        for (int j = 0; j < COLUMNAS_MATRIZ_DEF; j++){
-            printf("%d ", producto[i][j]);
-        }
-        printf("\n");
-    }
-
-
-   
-    
+int min(int arreglo[FILAS_MATRIZ]){
+  int min = arreglo[0];
+  for (int i = 0; i < FILAS_MATRIZ; i++) {
+    if(arreglo[i] < min && arreglo[i] > 0)
+      min = arreglo[i];
+  }
+  return min;
 }
 
 
+int main(int argc, char* argv[]) {
+
+  if(argc == 1)//Si no hay argumentos solo hace matriz cuadrada
+    potencia = 2;
+  else
+    potencia = atoi(argv[1]);
+   /*nt final[FILAS_MATRIZ_FINAL][COLUMNAS_MATRIZ];*/
+
+  memcpy(matrizB,matrizA, FILAS_MATRIZ * COLUMNAS_MATRIZ * sizeof(int));
+
+  for(int p = 1; p < potencia; p++){//cantidad de potencias
+    for (int a = 0; a < COLUMNAS_MATRIZ; a++){
+
+      for (int i = 0; i < FILAS_MATRIZ; i++){
+        int sumas[FILAS_MATRIZ];
+
+        for ( int j = 0; j < COLUMNAS_MATRIZ; j++){
+
+            sumas[j] = matrizA[i][j] * matrizB[j][a];//aqui!!
+        }
+        producto[i][a] = min(sumas);
+      }
+    }
+    memcpy(matrizB,producto, FILAS_MATRIZ * COLUMNAS_MATRIZ * sizeof(int));
+
+  }
+  printf("Matriz ^ %d\n",potencia);
+  imprimirMatriz(producto);
+  /*printf("MatrizA\n");//se puede borrar
+  imprimirMatriz(matrizA);
+  printf("MatrizB\n");
+  imprimirMatriz(matrizB);*/
 
 
-
-
-
-
+  return 0;
+}
